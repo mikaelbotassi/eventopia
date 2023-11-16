e7af05a8f49354335825431f595056e46acdd6e3
mikaelbotassi <elkamilive@gmail.com>
2023-11-16 13:57:07 -0300

    Structure: Inicial frontend's structure

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    fontFamily:{
+      sans: ['Poppins','sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
